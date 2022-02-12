4f3432a5ca6f95ba6a9653e8df259bc99ea8af41
Farrukh Karimov <farrukhkarimov@Farrukhs-MacBook-Pro.local>
2022-02-11 19:17:26 -0500

    tweaked button css styles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <h1>Counter: {counter}</h1>
-        <button 
-          onClick={() => setCounter(counter + 1)}
-        > + </button>
-        <button 
-          onClick={() => setCounter(0)}
-        > Reset </button>
-        { counter !== 0 && <button onClick={() => setCounter(counter - 1)}> - </button>}
-        
+
+        <button onClick={() => setCounter(counter + 1)}> + </button>
+
+        <button onClick={() => setCounter(0)}> Reset </button>
+
+        {counter !== 0 && (
+          <button onClick={() => setCounter(counter - 1)} className="btn btn-plus"> - </button>
+        )}
       </header>
     </div>
   );
